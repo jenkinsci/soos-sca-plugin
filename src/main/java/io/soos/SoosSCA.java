3092ede3a353f623d02cd2c5069a14b6917383b1
SOOS-JAlvarez <jalvarez@soos.io>
2023-08-22 10:01:51 -0300

    PA-10660 add contributing developers

--- src/main/java/io/soos/SoosSCA.java
+++ src/main/java/io/soos/SoosSCA.java
@@ -2,6 +2,10 @@
 package io.soos;
 import java.io.*;
 import java.util.*;
+
+import hudson.*;
+import hudson.model.*;
+import hudson.scm.ChangeLogSet;
 import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import hudson.util.Secret;
@@ -21,22 +25,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import hudson.EnvVars;
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.model.AbstractProject;
-import hudson.model.Result;
-import hudson.model.Run;
-import hudson.model.TaskListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import jenkins.tasks.SimpleBuildStep;
+
+import static com.sun.activation.registries.LogSupport.log;
 
 @Getter
 @Setter
@@ -86,7 +86,7 @@ public class SoosSCA extends Builder implements SimpleBuildStep {
             throws InterruptedException, IOException {
 
         Map<String, String> map = new HashMap<>();
-        map.putAll(populateContext(env));
+        map.putAll(populateContext(env, run));
         setEnvProperties(map);
         String resultURL = null;
         try {
@@ -214,7 +214,7 @@ public class SoosSCA extends Builder implements SimpleBuildStep {
         }
     }
 
-    private Map<String, String> populateContext(EnvVars env) {
+    private Map<String, String> populateContext(EnvVars env, Run<?, ?> run) throws AbortException {
         Map<String, String> map = new HashMap<>();
 
         String branchName = Utils.getBranchName(env.get(PluginConstants.GIT_BRANCH));
@@ -247,6 +247,12 @@ public class SoosSCA extends Builder implements SimpleBuildStep {
         if (StringUtils.isBlank(this.apiBaseURI)) {
             map.put(Constants.PARAM_API_BASE_URI_KEY, Constants.SOOS_DEFAULT_API_URL);
         }
+
+        final String user = getChangeUser(run);
+        if(!user.isEmpty()) {
+            map.put(Constants.PARAM_CONTRIBUTING_DEVELOPER_KEY, user);
+            map.put(Constants.PARAM_CONTRIBUTING_DEVELOPER_ENV_KEY, "JENKINS_ENTRY_USER");
+        }
         return map;
     }
 
@@ -276,4 +282,39 @@ public class SoosSCA extends Builder implements SimpleBuildStep {
         }
         return null;
     }
+
+    private String getChangeUser(Run<?, ?> run) throws AbortException {
+        String userName = null;
+        Cause.UserIdCause user = (Cause.UserIdCause) run.getCause(Cause.UserIdCause.class);
+        if (user != null && user.getUserName() != null) {
+            log("Build initiated by: " + user.getUserName());
+            if (!user.getUserName().contains("anonymous")) {
+                userName = user.getUserName();
+            }
+        }
+        if (userName == null) {
+            ItemGroup<?> ig = run.getParent().getParent();
+            nextItem: for (Item item : ig.getItems()) {
+                if (!item.getFullDisplayName().equals(run.getFullDisplayName())
+                        && !item.getFullDisplayName().equals(run.getParent().getFullDisplayName())) {
+                    continue;
+                }
+                for (Job<?, ?> job : item.getAllJobs()) {
+                    if (job instanceof AbstractProject<?, ?>) {
+                        AbstractProject<?, ?> p = (AbstractProject<?, ?>) job;
+                        for (AbstractBuild<?, ?> b : p.getBuilds()) {
+                            for (ChangeLogSet.Entry entry : b.getChangeSet()) {
+                                if (entry.getAuthor() != null) {
+                                    userName = entry.getAuthor().getFullName();
+                                    break nextItem;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return userName;
+    }
+
 }
